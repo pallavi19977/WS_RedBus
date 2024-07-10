232c5b28070d340ab925ad9186cab4a96facabcd
pallavi19977 <pallavibhutekar1997@gmail.com>
2024-07-10 13:57:24 +0530

    error cleared
    Error cleared

--- WS_RedBus/src/com/redbus/RedBus.java
+++ WS_RedBus/src/com/redbus/RedBus.java
@@ -1,12 +1,12 @@
 package com.redbus;
 
 public class RedBus {
- public static void main(String args[])
- {
 	 public void m1()
 	 {
 		System.out.println("Changed msg in this class added m1 method"); 
-	 }	
+	 }
+	 public static void main(String args[])
+		 {
 	 System.out.println("This is MS RedBus Project..!!");
 	 
  }
