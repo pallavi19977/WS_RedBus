3ecf92966e81e78fa401a4b23ca4b527e687ec5d
pallavi19977 <pallavibhutekar1997@gmail.com>
2024-07-10 13:54:32 +0530

    add m1 method
    Add m1 method to get data

--- WS_RedBus/src/com/redbus/RedBus.java
+++ WS_RedBus/src/com/redbus/RedBus.java
@@ -3,6 +3,11 @@ package com.redbus;
 public class RedBus {
  public static void main(String args[])
  {
+	 public void m1()
+	 {
+		System.out.println("Changed msg in this class added m1 method"); 
+	 }	
 	 System.out.println("This is MS RedBus Project..!!");
+	 
  }
 }
